0166c14ae3cb9fcf9ee3fc073322a900f976a2f6
Erik Tavares <erikalves0799@gmail.com>
2021-07-21 23:26:04 -0300

    New Version Home with CSS Animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,9 @@ import { AfterViewInit, Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements AfterViewInit {
-  ngAfterViewInit() {}
+  ngAfterViewInit() {
+    this.controlAnimationOnStart();
+  }
 
   onToggleClick() {
     let toggle = document.querySelector('.toggle');
@@ -46,6 +48,47 @@ export class AppComponent implements AfterViewInit {
       const { x, y } = button.getBoundingClientRect();
       button.style.setProperty('--x', `${event.clientX - x}`);
       button.style.setProperty('--y', `${event.clientY - y}`);
+    }
+  }
+
+  activeLink(id: any) {
+    let linksLi = document.querySelectorAll<HTMLElement>('.nav-item');
+    if (linksLi) {
+      linksLi.forEach((element: any) => {
+        element.classList.remove('active');
+      });
+      const actualLink = document.getElementById(id);
+      if (actualLink) actualLink.classList.add('active');
+    }
+  }
+
+  controlAnimationOnStart() {
+    let hi_title = document.getElementById('hi_title');
+    let i_am_title = document.getElementById('i_am_title');
+    let role_title = document.getElementById('role_title');
+    let btn = document.getElementById('download-cv-btn');
+    if (hi_title && i_am_title && role_title && btn) {
+      i_am_title.style.display = 'none';
+      role_title.style.display = 'none';
+      hi_title.classList.add('typewriter');
+
+      setTimeout(() => {
+        hi_title?.classList.remove('typewriter');
+        if (i_am_title) {
+          i_am_title.style.display = 'flex';
+          i_am_title.classList.add('typewriter');
+        }
+        setTimeout(() => {
+          if (i_am_title && role_title) {
+            i_am_title.classList.remove('typewriter');
+            role_title.style.display = 'flex';
+            role_title.classList.add('typewriter');
+            setTimeout(() => {
+              if (btn) btn.classList.add('active');
+            }, 2000);
+          }
+        }, 2000);
+      }, 2000);
     }
   }
 }
